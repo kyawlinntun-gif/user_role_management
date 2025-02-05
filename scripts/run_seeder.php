5b0b872a2dbd6c5a92990578d8672ca75deb4c81
kyawlinntun <59950395+kyawlinntun-gif@users.noreply.github.com>
2025-02-05 13:44:21 +0630

    Role process setup

--- scripts/run_seeder.php
+++ scripts/run_seeder.php
@@ -1,9 +1,12 @@
 <?php
 require_once __DIR__ . '/../App/Core/Database.php';
 require_once __DIR__ . '/../seeder/UserSeeder.php';
+require_once __DIR__ . '/../seeder/RoleSeeder.php';
 use App\Core\Database;
 // Run the seeder
 $database = new Database();
 $db = $database->getConnection();
-$seeder = new UserSeeder($db);
-$seeder->run();
+$roleSeeder = new RoleSeeder($db);
+$roleSeeder->run();
+$userSeeder = new UserSeeder($db);
+$userSeeder->run();
